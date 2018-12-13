7cf4ecd37f9aad41e01cc835c315759a4b77e19a
Aroha Dobson <aroha.dobson@arohamarieprogramming.com>
2018-12-13 15:45:52 -0700

    updated read me

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# friend-finder
+# Friend Finder
 
 Friend Finder is a survey application that collects and stores user data and matches the user with the most similar user based on the result algorithm.
 
-Technologies Used: Express, Node, Javascript, HTML, CSS, Bootstrap, jQuery
-
-Description:
+## About
 
 Users enter the application through a welcome page. Once in the survey form, users will input a name and a profile picture and fill out the survey. Once entered, the application searches through all previous form submissions and finds the most compatible match.
+
+## Code Languages
+
+    • JavaScript
+    • Node.js
+    • npm
+    • MySQL
+    • Express
+    • JQuery
+    • CSS
+    • Heroku
+
+## Deployed Site
+
+[Friend Finder!](https://young-spire-20600.herokuapp.com/)
+
+## Gifs
+
+### Survey Section
+
+![Friend Finder runthrough](https://media.giphy.com/media/iBb6OLMdckzUFcPKYs/giphy.gif)
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,41 +1,41 @@
 var friends = require("../data/friends");
 
 module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-	app.get("/api/friends", function(req, res) {
-		res.json(friends)
-	});
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body);
+    var user = req.body;
+    var userScores = user.scores;
 
-	app.post("/api/friends", function (req, res) {
-		console.log(req.body);
-		var user = req.body;
-		var userScores = user.scores;
+    var bestMatch = friends[0];
+    var bestScore = 100;
+    var compScore = 0;
 
-				var bestMatch = friends[0];
-				var bestScore = 100;
-				var compScore = 0;
-
-				for (var i in friends) {
-					for (var j = 0; j <10; j++) {
-						compScore += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
-						console.log(compScore);
-					}
-					if ((bestScore > compScore) && (user.name != friends[i].name)) {
-						bestMatch = friends[i];
-						bestScore = compScore;
-					}
-					compScore = 0;
-				}
-		var nameFlag = true;
-		for (var i = 0; i < friends.length; i++) {
-			if (user.name == friends[i].name) {
-				nameFlag = false;
-			}
-		}
-		if (nameFlag) {
-			friends.push(req.body);
-		}
-		res.json(bestMatch);
-	});
-
-}
+    for (var i in friends) {
+      for (var j = 0; j < 10; j++) {
+        compScore += Math.abs(
+          parseInt(friends[i].scores[j]) - parseInt(userScores[j])
+        );
+        console.log(compScore);
+      }
+      if (bestScore > compScore && user.name != friends[i].name) {
+        bestMatch = friends[i];
+        bestScore = compScore;
+      }
+      compScore = 0;
+    }
+    var nameFlag = true;
+    for (var i = 0; i < friends.length; i++) {
+      if (user.name == friends[i].name) {
+        nameFlag = false;
+      }
+    }
+    if (nameFlag) {
+      friends.push(req.body);
+    }
+    res.json(bestMatch);
+  });
+};
